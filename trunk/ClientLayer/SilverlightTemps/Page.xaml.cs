49e11bad6bbc7228abe0642ad41c73239dd5cd9d
garybort <garybort@8d6bc988-f3a5-11dd-9d1d-173cc1d06e3c>
2009-03-01 06:42:51 +0000

    Initial load of Silverlight demo app

---
+++ trunk/ClientLayer/SilverlightTemps/Page.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightTemps
+{
+    public partial class Page : UserControl
+    {
+        private List<TemperatureDay> TempDays;
+        private int selectedDayIndex;
+        private TemperatureDay selectedDay;
+        public Page()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(Page_Loaded);
+            this.NextBtn.Click += new RoutedEventHandler(NextBtn_Click);
+            this.PrevBtn.Click += new RoutedEventHandler(PrevBtn_Click);
+        }
+
+        void PrevBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.SelectedDayIndex--;
+        }
+
+        void NextBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.SelectedDayIndex++;
+        }
+
+        void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataService.TemperatureDataService ds = new DataService.MockTemperatureDataService();
+            ds.GetRecentTemperaturesComplete += new SilverlightTemps.DataService.TemperatureDataEventHandler(ds_GetRecentTemperaturesComplete);
+            ds.GetRecentTemperaturesAsync("72434013994", 700);
+        }
+
+        void ds_GetRecentTemperaturesComplete(object sender, SilverlightTemps.DataService.TemperatureDataEventArgs e)
+        {
+            this.TempDays = e.TemperatureDays;
+            if (this.TempDays.Count > 0)
+            {
+                this.SelectedDayIndex = TempDays.Count - 1;
+            }
+            this.LoadingPanel.Visibility = Visibility.Collapsed;
+        }
+        int SelectedDayIndex
+        {
+            get
+            {
+                return this.selectedDayIndex;
+            }
+            set
+            {
+                this.selectedDayIndex = value;
+                this.selectedDay = TempDays[this.selectedDayIndex];
+                this.DataContext = this.selectedDay;
+                this.PrevBtn.IsEnabled = this.selectedDayIndex > 0;
+                this.NextBtn.IsEnabled = this.selectedDayIndex < this.TempDays.Count() - 1;
+                this.RangeGauge1.SubjectTemperatureDay = this.selectedDay;
+                this.RangeGauge1.ThumbTransition.Begin();
+                this.GrowTemps.Begin();
+            }
+        }
+    }
+}
---
+++ trunk/ClientLayer/SilverlightTemps/RangeGauge.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace SilverlightTemps
+{
+    public partial class RangeGauge : UserControl
+    {
+        const double GaugeMin = -20;
+        const double GaugeMax = 110;
+        public RangeGauge()
+        {
+            InitializeComponent();
+            this.ThumbRectangle.RenderTransformOrigin = new Point(0.5, 0.5);
+        }
+
+        public double GaugeWidth
+        {
+            get { return (double)GetValue(GaugeWidthProperty); }
+            set { SetValue(GaugeWidthProperty, value); this.GridBorder.Width = value; }
+        }
+
+        public static readonly DependencyProperty GaugeWidthProperty = DependencyProperty.Register("GaugeWidth", typeof(double), typeof(RangeGauge), new PropertyMetadata(Convert.ToDouble(300)));
+
+        private TemperatureDay subject;
+        public TemperatureDay SubjectTemperatureDay
+        {
+            get { return this.subject; }
+            set
+            {
+                this.subject = value;
+                RedrawThumb();
+            }
+        }
+
+        void RedrawThumb()
+        {
+            if (subject != null)
+            {
+                double GaugeDegrees = GaugeMax - GaugeMin;
+                this.GridBorder.Width = this.GaugeWidth;
+                this.ThumbTranslate.X = (((subject.MinTemperature - GaugeMin) / GaugeDegrees) * this.GridBorder.Width);
+                this.ThumbRectangle.Width = (((subject.MaxTemperature - subject.MinTemperature) / GaugeDegrees) * this.GridBorder.Width);
+            }
+        }
+    }
+}
